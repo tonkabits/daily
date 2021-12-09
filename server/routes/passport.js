12dee2db8732ba09587c010cac3ab3979c892ca5
Tonkabits <tonkabits@gmail.com>
2021-12-09 13:49:26 -0500

    revert to standalone passport and env Creds

--- server/routes/passport.js
+++ server/routes/passport.js
@@ -2,6 +2,21 @@ const passport = require("passport")
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require("../models/User.model");
 const findOrCreate = require('mongoose-findorcreate')
+const router = require("express").Router();
+
+// ℹ️ Handles password encryption
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
+const saltRounds = 10;
+
+// Require the User model in order to interact with the database
+
+const Session = require("../models/Session.model");
+
+
+
 passport.serializeUser(function (user, done) {
     done(null, user);
 });
@@ -10,10 +25,18 @@ passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
+
+router.get('/google',
+    passport.authenticate('google', {
+        scope:
+            ['email', 'profile']
+    })
+)
+
 passport.use(new GoogleStrategy({
-    clientID: "518153544404-dfasll0ria6qpoivih46rht0emjeskgd.apps.googleusercontent.com",
-    clientSecret: "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",
-    callbackURL: "http://localhost:5005/google",
+    clientID: process.env.PASSPORT_GOOGLE_CLIENT_ID,
+    clientSecret: process.env.PASSPORT_GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.PASSPORT_GOOGLE_CLIENT_CALLBACK_URL,
     passReqToCallback: true
 },
     function (request, accessToken, refreshToken, profile, done) {
